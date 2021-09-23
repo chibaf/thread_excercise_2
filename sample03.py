711c5d3d0703ac39533f654e23fb7c64562cc71c
chibaf <chibaf@mac.com>
2021-09-23 20:02:33 +0900

    Add files via upload

---
+++ sample03.py
@@ -0,0 +1,22 @@
+import threading
+import queue, time
+
+def thread1(a,q):
+    time.sleep(3)
+    ret = [1,2]
+    q.put(ret)
+
+q =queue.Queue()
+th = threading.Thread(target=thread1, args=(1,q),daemon=True)
+th.start()
+#th.join()
+while True:
+  if threading.active_count()==1:
+    rslt = q.get()
+    print(rslt)
+    print('thread ended')
+    break
+  print('not end')
+  time.sleep(1)
+
+exit()
